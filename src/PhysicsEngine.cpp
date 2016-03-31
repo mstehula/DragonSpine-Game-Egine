030ad8506f6c2c8351350b15f4679771f893177a
Mitch <mstehula@gmail.com>
2016-03-30 23:39:45 -0400

    Added physics objects to allow for simulation, have them go through list and simulate all objects (possibly graph like structure to get everyone around it)

--- src/PhysicsEngine.cpp
+++ src/PhysicsEngine.cpp
@@ -2,15 +2,39 @@
 
 namespace dragonspinegameengine
 {
+    PhysicsEngine* PhysicsEngine::instance_;
+    PhysicsEngine* PhysicsEngine::GetInstance()
+    {
+        if(instance_ == nullptr)
+        {
+            instance_ = new PhysicsEngine();
+        }
+        return instance_;
+    }
 
     PhysicsEngine::PhysicsEngine()
     {
         //ctor
     }
 
-    PhysicsEngine::~PhysicsEngine()
+    void PhysicsEngine::Simulate(PhysicsObject* physics_object)
     {
-        //dtor
+        physics_object->Simulate();
+    }
+
+    void PhysicsObject::Simulate()
+    {
+        position_ = position_ + velocity_;
+    }
+
+    void PhysicsObject::ApplyForce(float x_force, float y_force, float z_force)
+    {
+
+    }
+
+    void PhysicsObject::ApplyForceDir(float front_force, float side_force, float up_force)
+    {
+
     }
 
 }
