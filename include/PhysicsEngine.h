e0a5c96f0f4d4db5504cf22f0839174652d47e65
Mitch <mstehula@gmail.com>
2016-03-30 23:39:45 -0400

    Added physics objects to allow for simulation, have them go through list and simulate all objects (possibly graph like structure to get everyone around it)

--- include/PhysicsEngine.h
+++ include/PhysicsEngine.h
@@ -1,18 +1,39 @@
 #ifndef PHYSICSENGINE_H
 #define PHYSICSENGINE_H
 
+#include <glm/vec3.hpp>
 
 namespace dragonspinegameengine
 {
+
+    class PhysicsObject;
+
     class PhysicsEngine
     {
         public:
-            PhysicsEngine();
-            virtual ~PhysicsEngine();
+            static PhysicsEngine* GetInstance();
 
-        protected:
-
+            void Simulate(PhysicsObject* physics_object);
         private:
+            static PhysicsEngine* instance_;
+
+            PhysicsEngine();
+    };
+
+    class PhysicsObject
+    {
+        public:
+            void Simulate();
+
+            void ApplyForce(float x_force, float y_force, float z_force);
+            void ApplyForceDir(float front_froce, float side_force, float up_force);
+            void Stop();
+        private:
+            glm::vec3 position_;
+            glm::vec3 velocity_;
+            glm::vec3 direction_;
+
+            float mass_;
     };
 
 }
