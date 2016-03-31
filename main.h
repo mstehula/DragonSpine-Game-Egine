8eb2461ebadf2ab3d982f30b6f08c1d146660ce2
Mitch <mstehula@gmail.com>
2016-03-31 00:16:12 -0400

    Added a main file to move all testing into and out of the base engine class. This way it will simulate how the accesses will happen outside the engine

---
+++ main.h
@@ -0,0 +1,13 @@
+///TEST CLASS FOR DRAGON SPINE GAME ENGINE
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#include "PhysicsEngine.h"
+
+class Entity: public dragonspinegameengine::PhysicsObject
+{
+
+};
+
+#endif // MAIN_H
