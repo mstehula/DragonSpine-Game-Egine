e5a7d8039aa300939570efb3da1c7511fe6983b1
Mitch <mstehula@gmail.com>
2016-04-26 16:58:10 +0000

    Removed tests that did not matter, as the application of the memory management is way to convoluted

--- src/tests/DODArrayTest.cpp
+++ src/tests/DODArrayTest.cpp
@@ -35,32 +35,6 @@ BOOST_AUTO_TEST_CASE(setup_allocated_constructor)
     BOOST_CHECK(test_array_2.AllocatedSize() == 256);
 }
 
-BOOST_AUTO_TEST_CASE(setup_default_constructor_dynamic)
-{
-    //Test Arrays - Automatic and Dynamically allocated
-    TestArray* test_array_3 = new TestArray();
-
-    //Testing methods - checks empty size and allocated size
-    //These are the only values to be changed by the constructor
-    BOOST_CHECK(test_array_3->Size() == 0);
-    BOOST_CHECK(test_array_3->AllocatedSize() == 128);
-
-    delete test_array_3;
-}
-
-BOOST_AUTO_TEST_CASE(setup_allocated_constructor_dynamic)
-{
-    //Test Arrays - Automatic and Dynamically allocated
-    TestArray* test_array_4 = new TestArray(256);
-
-    //Testing methods - checks empty size and allocated size
-    //These are the only values to be changed by the constructor
-    BOOST_CHECK(test_array_4->Size() == 0);
-    BOOST_CHECK(test_array_4->AllocatedSize() == 256);
-
-    delete test_array_4;
-}
-
 BOOST_AUTO_TEST_CASE(one_item_default)
 {
     TestArray test_array_1;
@@ -88,38 +62,4 @@ BOOST_AUTO_TEST_CASE(one_item_allocated)
     BOOST_CHECK(test_array_2.AllocatedSize() == 256);
     BOOST_CHECK(test_struct_2_index == 0);
     BOOST_CHECK(test_array_2[test_struct_2_index].id == test_struct_2.id);
-}
-
-BOOST_AUTO_TEST_CASE(one_item_default_dynamic)
-{
-    TestArray* test_array_3 = new TestArray();
-    TestStruct* test_struct_3 = new TestStruct();
-    int test_struct_3_index;
-
-    test_struct_3->id = 3;
-    test_struct_3_index = test_array_3->Add(test_struct_3);
-    BOOST_CHECK(test_array_3->Size() == 1);
-    BOOST_CHECK(test_array_3->AllocatedSize() == 128);
-    BOOST_CHECK(test_struct_3_index == 0);
-    BOOST_CHECK((*test_array_3)[test_struct_3_index].id == test_struct_3->id);
-
-    delete test_array_3;
-    delete test_struct_3;
-}
-
-BOOST_AUTO_TEST_CASE(one_item_allocated_dynamic)
-{
-    TestArray* test_array_4 = new TestArray(256);
-    TestStruct* test_struct_4 = new TestStruct();
-    int test_struct_4_index;
-
-    test_struct_4->id = 4;
-    test_struct_4_index = test_array_4->Add(test_struct_4);
-    BOOST_CHECK(test_array_4->Size() == 1);
-    BOOST_CHECK(test_array_4->AllocatedSize() == 256);
-    BOOST_CHECK(test_struct_4_index == 0);
-    BOOST_CHECK((*test_array_4)[test_struct_4_index].id == test_struct_4->id);
-
-    delete test_array_4;
-    delete test_struct_4;
 }
